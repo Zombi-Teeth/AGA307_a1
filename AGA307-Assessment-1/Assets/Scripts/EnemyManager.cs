11297263e2fa5b5fa30593603e19ca14aa4bd65b
Zombi-Teeth <mzurvas1@myjmc.edu.au>
2024-06-17 16:20:02 +1000

    enemy Spawning

---
+++ AGA307-Assessment-1/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Transform[] spawnPoints = new Transform[8];
+    public GameObject[] enemyTypes = new GameObject[8];
+
+    public List<GameObject> enemies = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
